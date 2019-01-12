1ba8bd20b519972da4a10947dd5d0ee268bca5e4
Jerko Steiner <jerko.steiner@gmail.com>
2019-01-12 13:22:54 +0100

    Remove jest.setup.js

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var config_1 = require("./src/config");
-var NoopLogger_1 = require("./src/NoopLogger");
-config_1.setLogger(new NoopLogger_1.NoopLogger());
